43e63fd19829d120c74db6c4252ece393ffac367
Neethu Isaac <neethuisaac@gmail.com>
2022-03-29 15:28:16 +0530

    Adding a seeder for importing from sql file

---
+++ database/seeders/ImportFromFileSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ImportFromFileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $file_name = resource_path('database').'/blog.sql';
+        echo $file_name;
+        print_r($_SERVER);
+
+        //exec("sudo mysql -u root --database blog2 < $file_name");
+    }
+}
