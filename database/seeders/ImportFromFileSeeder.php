5544023b91d9df50d5152ed2e8f566c7819dd0a4
tigabits <62597721+tigabits@users.noreply.github.com>
2022-03-29 19:59:18 +0530

    git ignore changed

--- database/seeders/ImportFromFileSeeder.php
+++ database/seeders/ImportFromFileSeeder.php
@@ -13,20 +13,22 @@ class ImportFromFileSeeder extends Seeder
      */
     public function run()
     {
-        $file_name = resource_path('database').'/blog.sql';
+        $file_name = resource_path('database').'\blog.sql';
         //echo $file_name;
         //print_r($_SERVER);
         $db_username = config('database.connections.mysql.username');
         $db_password = config('database.connections.mysql.password');
         $password_option = $db_password == ''? '' : " -p $db_password ";
         $db_database = config('database.connections.mysql.database');
-        $command = "mysql -u $db_username $password_option --database $db_database < $file_name";
-        //echo $command;
+        //$command = "mysql -u $db_username $password_option --database $db_database < $file_name";
+        $command = "Get-Content $file_name |  ".' C:\xampp\mysql\bin\mysql'." -u $db_username $password_option  --database $db_database";
+        echo $command;
 
         //$command = "sudo mysql -u root --database blog2 < $file_name"; // For Linux
-        if(!system($command)){
-            echo "Execution failed. Could not import database.";
-            return false;
-        }
+        exec($command);
+        // if(!system($command)){
+        //     echo "Execution failed. Could not import database.";
+        //     return false;
+        // }
     }
 }
